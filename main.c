5eeaf87880701487cad6f42e988505be2d1c4347
Joel <you@example.com>
2024-01-17 20:12:42 +0200

    Can open in curses

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
-#include <stdio.h>    
+#include <ncurses.h>
+#include <stdio.h>   
 #include <unistd.h>
 #include <limits.h>
 #include <string.h>
@@ -7,7 +8,8 @@ char *line;
 char input[255];
 char cwd[PATH_MAX];
 char *cwd_pointer = cwd;
- 
+int max_lines;
+
 void concatenate_string(char* s, char* s1)
 {
     int i;
@@ -23,36 +25,93 @@ void concatenate_string(char* s, char* s1)
     return;
 }
 
-int main(int argc, char *argv[]){
-	getcwd(cwd, sizeof(cwd));
+void renderFile(int line, int character, char* filepath)
+{
+	// Concatenate path to file, then open file
+	char path_to_file[PATH_MAX + strlen(filepath)];
+	strcpy(path_to_file, cwd_pointer);
+	concatenate_string(path_to_file, "/");
+	concatenate_string(path_to_file, filepath);
+	FILE* fp = fopen(path_to_file, "r");
+	
+	//Resetting max_lines as file might have changed
+	max_lines = 0;
+	
+	//Clear window
+	clear();
 
-	if (argc <= 0) {
+	if(fp != NULL) 
+	{
+		while (fgets(input, sizeof(input), fp) != NULL) 
+		{
+			if(input[0] != '\n')
+			{
+				max_lines++;
+				printw("%s\n",input);	
+			}
+		}
+		fclose(fp);
+	}
+	
+	refresh();			/* Print it on to the real screen */
+
+}
+
+int main(int argc, char *argv[])
+{
+	int line = 1; // What line are you editing
+	int character = 1; // What character are you editing
+	int c;
+	int eject = 0;
+
+	// If no arguments, exit
+	if (argc <= 0) 
+	{
 		return 0;	
 	}
 
-	for (int i = 0; i < argc; i++) {
-		printf("%s\n", argv[i]);
-	}
+	//Get current DIR
+	getcwd(cwd, sizeof(cwd));
 	
-	// Add this shit to the path
-	char path_to_file[PATH_MAX + strlen(argv[1])];
-	strcpy(path_to_file, cwd_pointer);
-	concatenate_string(path_to_file, "/");
-	concatenate_string(path_to_file, argv[1]);
+	initscr();			/* Start curses mode 		*/
+	raw();				/* Line buffering disabled	*/
+	keypad(stdscr, TRUE);		/* We get F1, F2 etc..		*/
+	noecho();			/* Don't echo() while we do getch */
 	
-	printf("%s\n",path_to_file);
+	//First render
+	renderFile(line, character, argv[1]);
 
-	FILE* fp = fopen(path_to_file, "r");
-	
-	if(fp != NULL) {
-		// do something
-
-		while (fgets(input, sizeof(input), fp) != NULL) {
-			printf("%s\n",input);
+	while(1)
+	{
+		//check inputs
+		c = getch();
+		switch(c)
+		{	case KEY_UP:
+				if(line != 1)
+					--line;
+				break;
+			case KEY_DOWN:
+				if(line != max_lines)
+					++line;
+				break;
+			case 3:
+				eject = 1;
+				break;
+			default:
+				mvprintw(24, 0, "Charcter pressed is = %3d Hopefully it can be printed as '%c'", c, c);
+				refresh();
+				break;
 		}
-				
-		fclose(fp);
-	}
+		
+		if (eject) {
+			break;
+		}
+		//render with new info
+		renderFile(line, character, argv[1]);
 
+	}	
+
+	endwin();			/* End curses mode		  */
+	
 	return 0;
 }  
